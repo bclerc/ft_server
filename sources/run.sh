a3b6a6676de70a3da75b9b766d175f55cc3f7ed7
bclerc <bclerc@student.42.fr>
2021-02-02 14:11:40 +0100

    finish

--- sources/run.sh
+++ sources/run.sh
@@ -7,13 +7,15 @@ newDbPassword='admin'
 newDb='wordpress'
 host=localhost
  
+echo 'Set autoindex'
+sed -i 's/INDEX/'"$autoindex"'/g' /etc/nginx/conf.d/default.conf
 echo 'Create user and wordpress database'
-
 commands="CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON *.* TO '${newUser}'@'${host}';GRANT ALL privileges ON \`${newDb}\`.*
-TO '${newUser}'@'${host}';FLUSH PRIVILEGES;"
+TO '${newUser}'@'${host}';FLUSH PRIVILEGES;" || echo 'Database already set'
 
 echo "${commands}" | /usr/bin/mysql -u root
 echo "Launch php service"
 service php7.3-fpm start
+echo "PHP is running ..."
 echo "Running ..."
 nginx -g 'daemon off;'
